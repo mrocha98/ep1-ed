834f0fbae9854f68f3abcf1895b1fbfd0414fb26
Matheus Rocha <matheus_rocha98@outlook.com>
2021-03-23 13:09:36 -0300

    first commit

---
+++ ep1_ed/app.py
@@ -0,0 +1,4 @@
+from random import randint
+
+def main():
+  print(f'RANDOM: {randint(1, 101)}')
---
+++ main.py
@@ -0,0 +1,6 @@
+from ep1_ed import app
+
+script = app.main
+
+if __name__ == "__main__":
+    script
