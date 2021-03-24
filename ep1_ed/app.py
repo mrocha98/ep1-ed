b81de280291bbabea10f81a73bde678159d1132b
Matheus Rocha <matheus_rocha98@outlook.com>
2021-03-24 17:35:05 -0300

    coisa pakas

--- ep1_ed/app.py
+++ ep1_ed/app.py
@@ -1,4 +1,36 @@
-from random import randint
+from time import time
+
+from ep1_ed.ordering_algorithms import mergesort, quicksort, selection, native, OrderingAlgorithm
+from ep1_ed.ordering_execution import OrderingExecution
+
+def countTimeSpendWith(algorithm, items):
+    start = time()
+    algorithm(items)
+    end = time()
+
+    return end - start
 
 def main():
-  print(f'RANDOM: {randint(1, 101)}')
+  numbers = []
+  scores = {
+    OrderingAlgorithm.MERGESORT: OrderingExecution(),
+    OrderingAlgorithm.QUICKSORT: OrderingExecution(),
+    OrderingAlgorithm.SELECTION: OrderingExecution(),
+    OrderingAlgorithm.NATIVE: OrderingExecution()
+  }
+  t_start = time()
+  t_end = time() + 5
+  quantity = 2000
+  while time() < t_end:
+    print(f'QTD: {quantity}')
+
+    numbers.extend(OrderingExecution.generateSource())
+
+    # print(f'mergesort: {round(countTimeSpendWith(mergesort, numbers), 3)}')
+    # print(f'quicksort: {round(countTimeSpendWith(quicksort, numbers), 3)}')
+    # print(f'selection: {round(countTimeSpendWith(selection, numbers), 3)}')
+    # print(f'native: {round(countTimeSpendWith(native, numbers), 5)}')
+
+    quantity += 2000
+
+  print(f'time elapsed: {time() - t_start}')
---
+++ ep1_ed/ordering_algorithms.py
@@ -0,0 +1,51 @@
+from typing import List
+from enum import Enum
+
+class OrderingAlgorithm(Enum):
+  MERGESORT = 'mergesort'
+  QUICKSORT = 'quicksort'
+  SELECTION = 'selection'
+  NATIVE = 'native'
+
+def mergesort(v: List):
+  if len(v) <= 1: return v
+  else:
+    m = len(v) // 2
+    e = mergesort(v[:m])
+    d = mergesort(v[m:])
+    return merge(e, d)
+
+def merge(e, d):
+  r = []
+  i, j = 0, 0
+  while i < len(e) and j < len(d):
+    if e[i] <= d[j]:
+      r.append(e[i])
+      i += 1
+    else:
+      r.append(d[j])
+      j += 1
+  r += e[i:]
+  r += d[j:]
+  return r
+
+def quicksort(v: List):
+  if len(v) <= 1: return v
+  pivô = v[0]
+  iguais  = [x for x in v if x == pivô]
+  menores = [x for x in v if x <  pivô]
+  maiores = [x for x in v if x >  pivô]
+  return quicksort(menores) + iguais + quicksort(maiores)
+
+def selection(v: List):
+  r = []
+  while v:
+    m = min(v)
+    r.append(m)
+    v.remove(m)
+  return r
+
+def native(v: List):
+  items = v.copy()
+  items.sort()
+  return items
